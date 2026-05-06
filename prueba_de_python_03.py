52e871bfee42b2de823b6d756f4659ef997b2a5e
pau <paula.martilloq@gmail.com>
2026-05-05 23:46:07 -0500

    Elaboracion de algoritmo en Pythonpython3

---
+++ prueba_de_python_03.py
@@ -0,0 +1,4 @@
+# Genere 2 arreglos paralelos que representen las sucursales de una empresa y sus ventas. Existen 25 sucursales en la empresa. Presente el promedio de ventas, así como las sucursales con ventas mayores al promedio. 
+
+sucursales = [i for i in range(25)]
+print(sucursales)
