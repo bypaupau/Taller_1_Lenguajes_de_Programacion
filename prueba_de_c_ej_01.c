be2839cabc24ad310e380a2f8822ff769f37cbaf
pau <paula.martilloq@gmail.com>
2026-05-05 21:49:05 -0500

    Creacion de las actividades

---
+++ prueba_de_c_ej_01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(){
+    int valor;
+    int suma = 0;
+    float promedio;
+
+
+   for(int i = 0; i < 10; i ++){
+    printf("Ingresar el valor %d: ", i + 1);
+    scanf("%d", &valor);
+    suma = suma + valor;
+   }
+
+    promedio = (float)suma / 10;
+    printf("\n La suma total es: %d\n", suma);
+    printf("El promedio es: %.2f\n", promedio);
+
+    return 0;
+}
